eb4abd63b141f35e0cc33a47a68c3e270d078990
vivdaxiang <bmd1989@163.com>
2017-02-27 00:53:09 +0800

    解决APP启动时白屏问题；

--- app/src/main/java/com/daxiang/taojin/ui/SplashActivity.java
+++ app/src/main/java/com/daxiang/taojin/ui/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        getWindow().setBackgroundDrawable(null);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
